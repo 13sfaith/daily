fbe62f79dd1b0652262f2a4348b0f5d017531517
13sfaith <13sfaith@gmail.com>
2020-09-06 12:42:03 -0700

    started new problem 9/6

---
+++ 10/prob.py
@@ -0,0 +1,15 @@
+'''
+Make a simple calculator using only {single ints, +, -, ()}
+
+example
+
+input: '- ( 3 + ( 2 - 1 ) )'
+output: -4
+'''
+
+def expToPost(exp):
+    
+
+
+def eval(exp):
+    input = expToPost(exp.split(' '))
